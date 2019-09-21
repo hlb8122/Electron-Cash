ecb0dc0e9ad51d0283329b642df096d99083ab53
Harry Barber <hlbarber93@gmail.com>
2019-09-21 16:38:37 +0700

    Add forms representing parsing and unknown kinds

--- gui/qt/keyserver/download_forms.py
+++ gui/qt/keyserver/download_forms.py
@@ -1,3 +1,4 @@
+import base64
 from PyQt5.QtWidgets import *
 from ..util import *
 from electroncash.i18n import _
@@ -11,28 +12,32 @@ def construct_download_forms(parent, extracted):
 
     forms = [OverviewForm(parent, extracted)]
     for entry in extracted.metadata.payload.entries:
-        if entry.kind == "text_utf8":
-            text = entry.entry_data.decode('utf8')
-            forms.append(DPlainTextForm(text))
-        elif entry.kind == "telegram":
-            handle = entry.entry_data.decode('utf8')
-            forms.append(DTelegramForm(handle))
-        elif entry.kind == "ks_urls":
-            pb = Keyservers.FromString(parent, entry.entry_data)
-            urls = list(pb.urls)
-            forms.append(DKeyserverURLForm(urls))
-        elif entry.kind == "vcard":
-            text = entry.entry_data.decode('utf8')
-            vcard = vobject.readOne(text, validate=True)
-            forms.append(DVCardForm(vcard))
-        elif entry.kind == "pubkey":
-            forms.append(DPubKeyForm(parent, entry.entry_data))
+        try:
+            if entry.kind == "text_utf8":
+                text = entry.entry_data.decode('utf8')
+                forms.append(DPlainTextForm(text))
+            elif entry.kind == "telegram":
+                handle = entry.entry_data.decode('utf8')
+                forms.append(DTelegramForm(handle))
+            elif entry.kind == "ks_urls":
+                pb = Keyservers.FromString(entry.entry_data)
+                urls = list(pb.urls)
+                forms.append(DKeyserverURLForm(parent, urls))
+            elif entry.kind == "vcard":
+                text = entry.entry_data.decode('utf8')
+                vcard = vobject.readOne(text, validate=True)
+                forms.append(DVCardForm(vcard))
+            elif entry.kind == "pubkey":
+                forms.append(DPubKeyForm(parent, entry.entry_data))
+            else:
+                forms.append(DUnknownForm(entry.kind, entry.entry_data))
+        except Exception as e:
+            forms.append(DErrorForm(entry.kind, e, entry.entry_data))
 
     return forms
 
 
 def pubkey_encrypt(parent, dest_pubkey: bytes):
-    import base64
     from ecdsa.ecdsa import curve_secp256k1, generator_secp256k1
     from ecdsa.util import string_to_number, number_to_string, randrange
     from electroncash.bitcoin import EC_KEY
@@ -46,11 +51,12 @@ def pubkey_encrypt(parent, dest_pubkey: bytes):
 
     plain_text = text_dialog(parent.top_level_window(),
                              "Message Encryption", "Plain text", "Ok")
-    entries = Entries(entries=[w2w_plain_text_entry(plain_text)])
-    encrypted_message = encrypt_entries(entries, src_pubkey, dest_pubkey)
-    encoded = str(base64.b64encode(encrypted_message))
-    parent.msg_box(QMessageBox.Information, None,
-                   "Cipher Text (Base 64)", encoded)
+    if plain_text:
+        entries = Entries(entries=[w2w_plain_text_entry(plain_text)])
+        encrypted_message = encrypt_entries(entries, src_pubkey, dest_pubkey)
+        encoded = str(base64.b64encode(encrypted_message))
+        parent.msg_box(QMessageBox.Information, None,
+                    "Cipher Text (Base 64)", encoded)
 
 
 class OverviewForm(QWidget):
@@ -64,54 +70,54 @@ class OverviewForm(QWidget):
         msg = _('URL the metadata was sourced from.')
         description_label = HelpLabel(_('&Source URL'), msg)
         overview_grid.addWidget(description_label, 0, 0)
-        self.source_url_text_e = QLineEdit()
-        self.source_url_text_e.setReadOnly(True)
-        self.source_url_text_e.setText(extracted.url)
-        description_label.setBuddy(self.source_url_text_e)
-        overview_grid.addWidget(self.source_url_text_e, 0, 1, 1, -1)
+        source_url_text_e = QLineEdit()
+        source_url_text_e.setReadOnly(True)
+        source_url_text_e.setText(extracted.url)
+        description_label.setBuddy(source_url_text_e)
+        overview_grid.addWidget(source_url_text_e, 0, 1, 1, -1)
 
         msg = _('Timestamp the metadata was uploaded.')
         description_label = HelpLabel(_('&Timestamp'), msg)
         overview_grid.addWidget(description_label, 1, 0)
-        self.dt_text_e = QLineEdit()
-        self.dt_text_e.setReadOnly(True)
+        dt_text_e = QLineEdit()
+        dt_text_e.setReadOnly(True)
         dt = datetime.utcfromtimestamp(
             extracted.metadata.payload.timestamp).strftime('%Y-%m-%d %H:%M:%S')
-        self.dt_text_e.setText(dt)
-        description_label.setBuddy(self.dt_text_e)
-        overview_grid.addWidget(self.dt_text_e, 1, 1, 1, -1)
+        dt_text_e.setText(dt)
+        description_label.setBuddy(dt_text_e)
+        overview_grid.addWidget(dt_text_e, 1, 1, 1, -1)
 
         msg = _('Expiry time the item.')
         description_label = HelpLabel(_('&Expiry'), msg)
         overview_grid.addWidget(description_label, 2, 0)
-        self.expiry_text_e = QLineEdit()
-        self.expiry_text_e.setReadOnly(True)
+        expiry_text_e = QLineEdit()
+        expiry_text_e.setReadOnly(True)
         expiry_time = extracted.metadata.payload.timestamp + extracted.metadata.payload.ttl
         dt = datetime.utcfromtimestamp(
             expiry_time).strftime('%Y-%m-%d %H:%M:%S')
-        self.expiry_text_e.setText(dt)
-        description_label.setBuddy(self.expiry_text_e)
-        overview_grid.addWidget(self.expiry_text_e, 2, 1, 1, -1)
+        expiry_text_e.setText(dt)
+        description_label.setBuddy(expiry_text_e)
+        overview_grid.addWidget(expiry_text_e, 2, 1, 1, -1)
 
         msg = _('Percentage of sampled nodes which have accepted the metadata.')
         description_label = HelpLabel(_('&Confidence'), msg)
         overview_grid.addWidget(description_label, 3, 0)
-        self.confidence_text_e = QLineEdit()
-        self.confidence_text_e.setReadOnly(True)
+        confidence_text_e = QLineEdit()
+        confidence_text_e.setReadOnly(True)
         percentage = str(round(float(extracted.confidence) * 100, 2)) + "%"
-        self.confidence_text_e.setText(percentage)
-        description_label.setBuddy(self.confidence_text_e)
-        overview_grid.addWidget(self.confidence_text_e, 3, 1, 1, -1)
+        confidence_text_e.setText(percentage)
+        description_label.setBuddy(confidence_text_e)
+        overview_grid.addWidget(confidence_text_e, 3, 1, 1, -1)
 
         msg = _('Public Key of the address.')
         description_label = HelpLabel(_('&Public Key'), msg)
         overview_grid.addWidget(description_label, 4, 0)
-        self.public_key_text_e = QLineEdit()
-        self.public_key_text_e.setReadOnly(True)
+        public_key_text_e = QLineEdit()
+        public_key_text_e.setReadOnly(True)
         pubkey_hex = extracted.metadata.pub_key.hex()
-        self.public_key_text_e.setText(pubkey_hex)
-        description_label.setBuddy(self.public_key_text_e)
-        overview_grid.addWidget(self.public_key_text_e, 4, 1, 1, -1)
+        public_key_text_e.setText(pubkey_hex)
+        description_label.setBuddy(public_key_text_e)
+        overview_grid.addWidget(public_key_text_e, 4, 1, 1, -1)
 
         encrypt = QPushButton(_("&Encrypt"))
         encrypt.clicked.connect(lambda: pubkey_encrypt(
@@ -121,20 +127,88 @@ class OverviewForm(QWidget):
         self.setLayout(overview_grid)
 
 
+class DUnknownForm(QWidget):
+    name = "Unknown"
+
+    def __init__(self, kind: str, raw: str, *args, **kwargs):
+        super(DUnknownForm, self).__init__(*args, **kwargs)
+        unknown_grid = QGridLayout()
+
+        msg = _('Kind of this entry.')
+        description_label = HelpLabel(_('&Kind'), msg)
+        unknown_grid.addWidget(description_label, 0, 0)
+        kind_text = QLineEdit()
+        kind_text.setReadOnly(True)
+        kind_text.setText(kind)
+        description_label.setBuddy(kind_text)
+        unknown_grid.addWidget(kind_text, 0, 1, 1, -1)
+
+        msg = _('Base 64 encoded bytes contained in the entry.')
+        description_label = HelpLabel(_('&Bytes'), msg)
+        unknown_grid.addWidget(description_label, 1, 0)
+        raw_text = QLineEdit()
+        raw_text.setReadOnly(True)
+        base64_str = str(base64.b64encode(raw))
+        raw_text.setText(base64_str)
+        description_label.setBuddy(raw_text)
+        unknown_grid.addWidget(raw_text, 1, 1, 1, -1)
+
+        self.setLayout(unknown_grid)
+
+
+class DErrorForm(QWidget):
+    name = "Error"
+
+    def __init__(self, kind: str, error: Exception, raw: str, *args, **kwargs):
+        super(DErrorForm, self).__init__(*args, **kwargs)
+        error_grid = QGridLayout()
+
+        msg = _('Kind of this entry.')
+        description_label = HelpLabel(_('&Kind'), msg)
+        error_grid.addWidget(description_label, 0, 0)
+        kind_text_e = QLineEdit()
+        kind_text_e.setReadOnly(True)
+        kind_text_e.setText(kind)
+        description_label.setBuddy(kind_text_e)
+        error_grid.addWidget(kind_text_e, 0, 1, 1, -1)
+
+        msg = _('Error that occured during parsing.')
+        description_label = HelpLabel(_('&Error'), msg)
+        error_grid.addWidget(description_label, 1, 0)
+        error_text_e = QLineEdit()
+        error_text_e.setReadOnly(True)
+        error_text_e.setText(str(error))
+        description_label.setBuddy(error_text_e)
+        error_grid.addWidget(error_text_e, 1, 1, 1, -1)
+
+        msg = _('Base 64 encoded bytes contained in the entry.')
+        description_label = HelpLabel(_('&Bytes'), msg)
+        error_grid.addWidget(description_label, 2, 0)
+        raw_text_e = QLineEdit()
+        raw_text_e.setReadOnly(True)
+        base64_str = str(base64.b64encode(raw))
+        raw_text_e.setText(base64_str)
+        description_label.setBuddy(raw_text_e)
+        error_grid.addWidget(raw_text_e, 2, 1, 1, -1)
+
+        self.setLayout(error_grid)
+
+
 class DPlainTextForm(QWidget):
     name = "Plain Text"
 
     def __init__(self, text: str, *args, **kwargs):
         super(DPlainTextForm, self).__init__(*args, **kwargs)
         plain_text_grid = QGridLayout()
+
         msg = _('Plain text contained in the entry.')
         description_label = HelpLabel(_('&Text'), msg)
         plain_text_grid.addWidget(description_label, 0, 0)
-        self.upload_plain_text_e = QTextEdit()
-        self.upload_plain_text_e.setReadOnly(True)
-        self.upload_plain_text_e.setText(text)
-        description_label.setBuddy(self.upload_plain_text_e)
-        plain_text_grid.addWidget(self.upload_plain_text_e, 0, 1, 1, -1)
+        upload_plain_text_e = QTextEdit()
+        upload_plain_text_e.setReadOnly(True)
+        upload_plain_text_e.setText(text)
+        description_label.setBuddy(upload_plain_text_e)
+        plain_text_grid.addWidget(upload_plain_text_e, 0, 1, 1, -1)
 
         self.setLayout(plain_text_grid)
 
@@ -145,14 +219,15 @@ class DTelegramForm(QWidget):
     def __init__(self, handle: str, *args, **kwargs):
         super(DTelegramForm, self).__init__(*args, **kwargs)
         telegram_grid = QGridLayout()
+
         msg = _('Telegram handle contained in the entry.')
         description_label = HelpLabel(_('&Handle'), msg)
         telegram_grid.addWidget(description_label, 0, 0)
-        self.upload_telegram_e = QLineEdit()
-        self.upload_telegram_e.setReadOnly(True)
-        self.upload_telegram_e.setText(handle)
-        description_label.setBuddy(self.upload_telegram_e)
-        telegram_grid.addWidget(self.upload_telegram_e, 0, 1, 1, -1)
+        upload_telegram_e = QLineEdit()
+        upload_telegram_e.setReadOnly(True)
+        upload_telegram_e.setText(handle)
+        description_label.setBuddy(upload_telegram_e)
+        telegram_grid.addWidget(upload_telegram_e, 0, 1, 1, -1)
 
         open_tg_button = QPushButton(_("&Open Telegram"))
 
@@ -170,14 +245,15 @@ class DPubKeyForm(QWidget):
     def __init__(self, parent, pubkey: bytes, *args, **kwargs):
         super(DPubKeyForm, self).__init__(*args, **kwargs)
         pubkey_grid = QGridLayout()
+
         msg = _('Plain text contained in the entry.')
         description_label = HelpLabel(_('&Public Key'), msg)
         pubkey_grid.addWidget(description_label, 0, 0)
-        self.upload_pubkey_e = QLineEdit()
-        self.upload_pubkey_e.setReadOnly(True)
-        self.upload_pubkey_e.setText(pubkey.hex())
-        description_label.setBuddy(self.upload_pubkey_e)
-        pubkey_grid.addWidget(self.upload_pubkey_e, 0, 1, 1, -1)
+        upload_pubkey_e = QLineEdit()
+        upload_pubkey_e.setReadOnly(True)
+        upload_pubkey_e.setText(pubkey.hex())
+        description_label.setBuddy(upload_pubkey_e)
+        pubkey_grid.addWidget(upload_pubkey_e, 0, 1, 1, -1)
 
         encrypt_button = QPushButton(_("&Encrypt"))
         pubkey_grid.addLayout(Buttons(encrypt_button), 1, 1, 1, -1)
@@ -195,11 +271,11 @@ class DKeyserverURLForm(QWidget):
         msg = _('Keyserver list contained in the entry.')
         description_label = HelpLabel(_('&Servers'), msg)
         keyserver_grid.addWidget(description_label, 0, 0)
-        self.upload_ks_urls_e = QTextEdit()
-        self.upload_ks_urls_e.setReadOnly(True)
-        self.upload_ks_urls_e.setText("\n".join(keyservers))
-        description_label.setBuddy(self.upload_ks_urls_e)
-        keyserver_grid.addWidget(self.upload_ks_urls_e, 0, 1, 1, -1)
+        upload_ks_urls_e = QTextEdit()
+        upload_ks_urls_e.setReadOnly(True)
+        upload_ks_urls_e.setText("\n".join(keyservers))
+        description_label.setBuddy(upload_ks_urls_e)
+        keyserver_grid.addWidget(upload_ks_urls_e, 0, 1, 1, -1)
 
         set_button = QPushButton(_("&Set Keyservers"))
 
@@ -221,29 +297,29 @@ class DVCardForm(QWidget):
         msg = _('Name of contact.')
         description_label = HelpLabel(_('&Name'), msg)
         vcard_grid.addWidget(description_label, 0, 0)
-        self.upload_vName_e = QLineEdit()
-        self.upload_vName_e.setReadOnly(True)
-        self.upload_vName_e.setText(card.fn.value)
-        description_label.setBuddy(self.upload_vName_e)
-        vcard_grid.addWidget(self.upload_vName_e, 0, 1, 1, -1)
+        upload_vName_e = QLineEdit()
+        upload_vName_e.setReadOnly(True)
+        upload_vName_e.setText(card.fn.value)
+        description_label.setBuddy(upload_vName_e)
+        vcard_grid.addWidget(upload_vName_e, 0, 1, 1, -1)
 
         msg = _('Mobile number of contact.')
         description_label = HelpLabel(_('&Mobile'), msg)
         vcard_grid.addWidget(description_label, 1, 0)
-        self.upload_vMobile_e = QLineEdit()
-        self.upload_vMobile_e.setReadOnly(True)
-        self.upload_vMobile_e.setText(card.tel.value)
-        description_label.setBuddy(self.upload_vMobile_e)
-        vcard_grid.addWidget(self.upload_vMobile_e, 1, 1, 1, -1)
+        upload_vMobile_e = QLineEdit()
+        upload_vMobile_e.setReadOnly(True)
+        upload_vMobile_e.setText(card.tel.value)
+        description_label.setBuddy(upload_vMobile_e)
+        vcard_grid.addWidget(upload_vMobile_e, 1, 1, 1, -1)
 
         msg = _('Email of contact.')
         description_label = HelpLabel(_('&Email'), msg)
         vcard_grid.addWidget(description_label, 2, 0)
-        self.upload_vEmail_e = QLineEdit()
-        self.upload_vEmail_e.setReadOnly(True)
-        self.upload_vEmail_e.setText(card.email.value)
-        description_label.setBuddy(self.upload_vEmail_e)
-        vcard_grid.addWidget(self.upload_vEmail_e, 2, 1, 1, -1)
+        upload_vEmail_e = QLineEdit()
+        upload_vEmail_e.setReadOnly(True)
+        upload_vEmail_e.setText(card.email.value)
+        description_label.setBuddy(upload_vEmail_e)
+        vcard_grid.addWidget(upload_vEmail_e, 2, 1, 1, -1)
 
         add_button = QPushButton(_("&Add Contact"))
         # TODO: Add contract
