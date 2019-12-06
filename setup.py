5831909707af32b0d6f456ca89b4716698a9c27c
Harry Barber <harrybarber@protonmail.com>
2019-12-07 01:15:13 +0700

    Add prefix server library setup files

--- setup.py
+++ setup.py
@@ -133,6 +133,7 @@ setup(
         'electroncash',
         'electroncash.locale',  # work-around for Android platform limitations
         'electroncash.qrreaders',
+        'electroncash.prefix_server',
         'electroncash.slp',
         'electroncash.utils',
         'electroncash_gui',
